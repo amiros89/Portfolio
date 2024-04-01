2ede97e6c4a3af78890257a1a6db8d306652d5dd
Amir Rashkovsky <amir.r@quali.com>
2024-04-01 16:41:44 +0300

    add happy number solution

---
+++ Python/Happy Number/main.py
@@ -0,0 +1,12 @@
+def is_happy(n):
+    hash = []
+    digits=[int(x) for x in str(n)]
+    result = sum([digit*digit for digit in digits])
+    while result != 1:
+        if result in hash:
+            return False 
+        else:
+            hash.append(result)
+            digits=[int(x) for x in str(result)]
+            result = sum([digit*digit for digit in digits])
+    return True
